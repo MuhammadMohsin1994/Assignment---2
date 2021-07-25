8f4ac3d279099da75b5a16cf59817e3d8c6cd39d
MuhammadMohsin1994 <justcallmohsin@gmail.com>
2021-07-25 16:01:36 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var Biodata = {
+name: "Muhammad Mohsin",
+email:  "justcallmohsin@gmail.com",
+password:"12345",
+age:"26",
+gender:"Male",
+city:"Karachi",
+country:"Pakistan"
+}
+
+if('age' in Biodata)
+{
+    console.log("Age Property Exist");
+}
+
+if('country' in Biodata)
+{
+    console.log("Country Property Exist");
+}
